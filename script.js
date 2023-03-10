fca3dbfb8136f26409714fe2aeb072ce40964437
Dhruv Gupta <77520689+Dhruv13g@users.noreply.github.com>
2023-03-10 14:01:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(" .multiple-text", {
+    strings: ["Full Stack Developer", "JAVA Developer","Athlete"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDe1ay: 1000,
+    loop: true
+});
+
